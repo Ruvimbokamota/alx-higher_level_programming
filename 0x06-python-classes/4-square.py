f1387d32b3352d4472ec6f40971520e6e58c5a3a
Ruvimbo Kamota <ruekamota@gmail.com>
2022-12-21 17:05:17 -0800

    remove indent n spaces

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -16,33 +16,33 @@ class Square:
         """
         self.size = size
 
-        def area(self):
-            """calculates the square's area
-            Returns:
-                The area of the square
-            """
-            return (self.__size) ** 2
-        
-        @property
-        def size(self):
-            """getter of __size
-            Returns:
-                The size of the square
-            """
-            return self.__size
+    def area(self):
+        """calculates the square's area
+        Returns:
+            The area of the square
+        """
+        return (self.__size) ** 2
 
-        @size.setter
-        def size(self, value):
-            """setter of __size
-            Args:
-                value (int): the size of a size of the square
-            Returns:
-                None
-            """
-            if type(value) is not int:
-                raise TypeError("size must be an integer")
+    @property
+    def size(self):
+        """getter of __size
+        Returns:
+            The size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter of __size
+        Args:
+            value (int): the size of a size of the square
+        Returns:
+            None
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
             else:
-                if value < 0:
-                    raise ValueError("size must be >= 0")
-                else:
-                    self.__size = value
+                self.__size = value
