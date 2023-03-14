fba3cab3883230ab8d48d39ab9239824a4b155b8
Ruvimbo Kamota <ruekamota@gmail.com>
2023-03-14 05:25:18 -0700

    square java

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,20 +1,14 @@
 #!/usr/bin/node
+const num = process.argv[2];
 
-/**
- * a script that prints a square
- */
- const inputSize = parseInt(process.argv[2]);
- let squareIndicator = '';
- 
- if (process.argv.length < 3 || isNaN(inputSize)) {
-   console.log('Missing size');
- }
- for (let i = 0; i < inputSize; i++) {
-   for (let j = 0; j < inputSize; j++) {
-     squareIndicator += 'X';
-   }
-   if (i !== inputSize - 1) {
-     squareIndicator += '\n';
-   }
- }
- console.log(squareIndicator);
+if (isNaN(num)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < num; i++) {
+    let msg = '';
+    for (let j = 0; j < num; j++) {
+      msg = msg + 'X';
+    }
+    console.log(msg);
+  }
+}
